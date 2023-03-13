2d625a470c962311d8f59350a34c746e2d7367e6
suriwong <suriwong@emetworks.com>
2023-03-13 18:09:27 +0700

    innitial

--- service/src/controller/webhook.controller.ts
+++ service/src/controller/webhook.controller.ts
@@ -1,4 +1,6 @@
+import { WebhookEvent } from "@line/bot-sdk";
 import { NextFunction, Request, Response } from "express";
+import webhookService from "../service/webhook.service";
 
 class WebhookController {
 
@@ -8,6 +10,50 @@ class WebhookController {
         res.status(200).json({ message: "ok" })
     }
     public handlePostWebHook = async (req: Request, res: Response, next: NextFunction) => {
+        const { body } = req
+        const handleEventData = await Promise.all(
+            body.events.map(async (event: WebhookEvent) => {
+                if (event.source.userId === 'Udeadbeefdeadbeefdeadbeefdeadbeef') {
+                    return;
+                }
+                switch (event.type) {
+                    case 'message':
+                        await webhookService.handleReplyTokenMessage(event.replyToken, [event.message])
+                        break;
+                    case 'follow':
+                        // await lineService.handleAddFriend(event);
+                        break;
+                    case 'unfollow':
+                        // await lineService.handleUnFriend(event);
+                        break;
+                    case 'join':
+                        // await lineService.handleAddGroup(event);
+                        break;
+                    case 'leave':
+                        // await lineService.handleLeaveGroup();
+                        break;
+                    case 'memberJoined':
+                        break;
+                    case 'memberLeft':
+                        break;
+                    case 'postback':
+                        // await lineService.handlePostBack();
+                        break;
+                    case 'beacon':
+                        break;
+                    case 'things':
+                        break;
+                    default:
+                        return;
+                }
+                if (event.source.type === 'group') {
+                    // return chatGroupService.getGroupByGroupId(dto);
+                } else {
+                    // return customerService.getCustomerByUserId(event.source.userId, channel._id);
+                }
+            })
+        );
+
         res.status(200).json({ message: "ok" })
     }
 }
--- service/src/service/webhook.service.ts
+++ service/src/service/webhook.service.ts
@@ -1,6 +1,21 @@
+import { Client, Message } from "@line/bot-sdk";
 
 class WebhookService {
+    private line: Client;
 
+    constructor() {
+        this.line = new Client({ channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '' });
+    }
+
+    handleReplyTokenMessage = async (replyToken: string, messages: Message[] | any): Promise<any> => {
+        try {
+            await this.line.replyMessage(replyToken, messages);
+        } catch (e) {
+            console.log(e)
+        }
+    };
 }
-const  webhookService = new WebhookService()
+
+
+const webhookService = new WebhookService()
 export default webhookService
